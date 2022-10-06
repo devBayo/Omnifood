20f3742263f2adcbe51e346ee2bf1f61777c4508
abel-otegbola <abel15655@gmail.com>
2022-10-06 20:10:44 +0100

    feat: Add functionality to navbar toggle

--- js/script.js
+++ js/script.js
@@ -3,5 +3,12 @@
 // Targeting Elements
 
 // Sticky Nav
+let nav = document.querySelector("header .main-nav")
+let navButton = document.querySelector("header .btn-mobile-nav") 
+
+navButton.addEventListener("click", () => {
+    nav.classList.toggle("active")
+    navButton.classList.toggle("active")
+})
 
 // Intersection Observer
