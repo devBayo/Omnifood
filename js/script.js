a408a66cb96b61b7746ffe03d95dad07dd45e3ee
Abdulqoyyum Ibrahim <71531531+devBayo@users.noreply.github.com>
2022-10-10 01:34:55 +0100

    Revert "feat: make navbar sticky by adding position fixed"

--- js/script.js
+++ js/script.js
@@ -3,12 +3,5 @@
 // Targeting Elements
 
 // Sticky Nav
-let nav = document.querySelector("header .main-nav")
-let navButton = document.querySelector("header .btn-mobile-nav") 
-
-navButton.addEventListener("click", () => {
-    nav.classList.toggle("active")
-    navButton.classList.toggle("active")
-})
 
 // Intersection Observer
